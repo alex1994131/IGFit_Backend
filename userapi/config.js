4b1bd3999f7ea6287179052e603f178165e1009d
ig <ig@fit.co>
2021-07-03 01:59:18 +0100

    changed mdb

--- userapi/config.js
+++ userapi/config.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose')
 
 // const dbconf = "mongodb://webpush_admin:Kiranku123%24@51.79.157.193:27731/webpush?serverSelectionTimeoutMS=5000&connectTimeoutMS=10000&authSource=webpush&authMechanism=SCRAM-SHA-256";
-const dbconf = "mongodb://localhost:3333/igfit?readPreference=primary&appname=MongoDB%20Compass&ssl=false"
+const dbconf = "mongodb://root:example_db_mongo@tim.immo:3333/igfit?authSource=admin&readPreference=primary&appname=MongoDB%20Compass"
 module.exports = {
     USER_SESSION_TIME: 900,
     eodhistorical_api: 'https://eodhistoricaldata.com/api/search/',
