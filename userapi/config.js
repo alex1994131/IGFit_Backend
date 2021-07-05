9002a1b9d5db6551e8bae4852071e538f1f016b7
Alex1994131 <79332584+Alex1994131@users.noreply.github.com>
2021-07-05 13:00:50 +0900

    change mongodb connection url

--- userapi/config.js
+++ userapi/config.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
-// const dbconf = "mongodb://root:example_db_mongo@tim.immo:3333/igfit?authSource=admin&readPreference=primary&appname=MongoDB%20Compass";
-const dbconf = "mongodb://localhost:3333/igfit?readPreference=primary&appname=MongoDB%20Compass&ssl=false"
+const dbconf = "mongodb://root:example_db_mongo@tim.immo:3333/igfit?authSource=admin&readPreference=primary&appname=MongoDB%20Compass";
+// const dbconf = "mongodb://localhost:3333/igfit?readPreference=primary&appname=MongoDB%20Compass&ssl=false"
 module.exports = {
     USER_SESSION_TIME: 900,
     eodhistorical_ticker_api: 'https://eodhistoricaldata.com/api/search/',
